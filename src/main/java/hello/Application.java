5372621b9ac1ec15b4fe236a082d152dfca31e5c
vimcmd <vim.cmd@gmail.com>
2016-06-19 12:56:27 +0500

    created configuration class

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,12 @@
+package hello;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * To upload files with Servlet 3.0 containers, you need to register a MultipartConfigElement class (which would be a
+ * <multipart-config> in web.xml). Spring Boot provides registration of that bean.
+ */
+@SpringBootApplication
+public class Application {
+    // Spring MVC Controller will be added soon.
+}
