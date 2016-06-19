fafb2eb2e0542a35a1836c88db2f3589358a08d4
vimcmd <vim.cmd@gmail.com>
2016-06-19 13:22:04 +0500

    make the application executable

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,6 +1,11 @@
 package hello;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.io.File;
 
 /**
  * To upload files with Servlet 3.0 containers, you need to register a MultipartConfigElement class (which would be a
@@ -8,5 +13,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class Application {
-    // Spring MVC Controller will be added soon.
+
+    public static String ROOT = "upload-dir";
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init() {
+        // crating root folder at startup
+        return (String[] args) -> new File(ROOT).mkdir();
+    }
+
 }
